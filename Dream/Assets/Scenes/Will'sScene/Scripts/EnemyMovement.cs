fb8169943dd2e303ae27e3f0d53a278fbc7bd1f9
willWallace-RIT <wg4466@rit.edu>
2020-12-12 10:06:56 -0500

    made enemy with simple path ready for dynamic instantiation

--- Dream/Assets/Scenes/Will'sScene/Scripts/EnemyMovement.cs
+++ Dream/Assets/Scenes/Will'sScene/Scripts/EnemyMovement.cs
@@ -10,37 +10,46 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      if(moveList ==null){
         moveList = new List<List<Vector3>>();
         List<Vector3> tempList = new List<Vector3>();
-        tempList.Add(new Vector3(-2,0,0));
-        tempList.Add(new Vector3(2,0,0));
+        tempList.Add(new Vector3(-2,1,0));
+        tempList.Add(new Vector3(2,1,0));
         moveList.Add(tempList);
         tempList = new List<Vector3>();
-        tempList.Add(new Vector3(2,0,0));
-        tempList.Add(new Vector3(2,0,2));
+        tempList.Add(new Vector3(2,1,0));
+        tempList.Add(new Vector3(2,1,2));
         moveList.Add(tempList);  
         tempList = new List<Vector3>();
-        tempList.Add(new Vector3(2,0,2));
-        tempList.Add(new Vector3(2,0,-2));
+        tempList.Add(new Vector3(2,1,2));
+        tempList.Add(new Vector3(2,1,-2));
         moveList.Add(tempList);
 
         tempList = new List<Vector3>();
-        tempList.Add(new Vector3(2,0,-2));
-        tempList.Add(new Vector3(-2,0,-2));
+        tempList.Add(new Vector3(2,1,-2));
+        tempList.Add(new Vector3(-2,1,-2));
         moveList.Add(tempList);
         tempList = new List<Vector3>();
-        tempList.Add(new Vector3(-2,0,-2));
-        tempList.Add(new Vector3(-2,0,0));
+        tempList.Add(new Vector3(-2,1,-2));
+        tempList.Add(new Vector3(-2,1,0));
         moveList.Add(tempList);
+      }
+      if(moveList.Count>1 && moveList[0][0].Equals(moveList[moveList.Count-1][1])){
         movementPath=new Path(moveList,true);
-        Debug.Log(movementPath.Index);
+        
 
+      }
+      else{
+      movementPath=new Path(moveList,false);
+      }
     }
 
     // Update is called once per frame
     void Update()
     {
         float dt = Time.deltaTime;
-        transform.position=movementPath.move(velocity*dt);
+        Vector3 move=movementPath.move(velocity*dt);
+    transform.forward=((move-transform.position).normalized);
+        transform.position = move;
     }
 }
