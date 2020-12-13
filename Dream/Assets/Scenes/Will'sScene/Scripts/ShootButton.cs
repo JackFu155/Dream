1c19019fec525a7ed99c1cecd96c1a50d3541a21
willWallace-RIT <wg4466@rit.edu>
2020-12-13 10:25:04 -0500

    added more to target scene

---
+++ Dream/Assets/Scenes/Will'sScene/Scripts/ShootButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootButton : MonoBehaviour
+{
+    public Material purp;
+    public Material green;
+    public bool activate=false;
+    private Renderer ren;
+    // Start is called before the first frame update
+    void Start()
+    {
+      ren = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void toggle(){
+      activate =!activate;
+      if(activate){
+        ren.material = green;
+      }else{
+        ren.material = purp;
+      }
+    }
+    void OnCollisionEnter(Collision col){
+      if(col.gameObject.tag == "Pillow"){
+        toggle();
+      }
+    }
+    
+}
---
+++ Dream/Assets/Scenes/Will'sScene/Scripts/TargetBuildGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBuildGameManager : MonoBehaviour
+{
+  public GameObject[] buttons;
+  public GameObject[] enemies;
+  public GameObject player;
+  public GameObject PillowPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PillowPrefab.GetComponent<PillowShoot>().player = player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      EnemyMovement eM;
+      if(Input.GetKeyDown(KeyCode.C)){
+        foreach(GameObject e in enemies){
+          eM=e.GetComponent<EnemyMovement>();
+          eM.aiSwap();
+        }
+      }
+      if(Input.GetKeyDown(KeyCode.Z)){
+        Instantiate(PillowPrefab);
+      }
+    }
+}
