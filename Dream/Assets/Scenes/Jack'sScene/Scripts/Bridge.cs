fc41003aca9b902088ed7c4b21e7b314de16f8ee
JackFu155 <jackfu155@gmail.com>
2020-12-13 16:13:17 -0500

    Commit

---
+++ Dream/Assets/Scenes/Jack'sScene/Scripts/Bridge.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public bool connected = false;
+    public bool rise = false;
+    public GameObject next;
+    public float startheight;
+    public float endheight;
+    public float raiseSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rise == true)
+        {
+            Rise();
+            if (connected == true)
+            {
+                if(transform.position.y >= startheight)
+                {
+                    next.GetComponent<Bridge>().rise = true;
+                }
+            }
+
+        }
+    }
+
+    public void Rise()
+    {
+        if(transform.position.y < endheight)
+        {
+            transform.position += new Vector3(0.0f, raiseSpeed, 0.0f);
+        }
+    }
+}
